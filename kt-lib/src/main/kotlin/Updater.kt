bf31fbc6efdbb5686a89ff855d97e2a8d734d459
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2020-11-19 01:12:01 +0300

    Version logic changed.

--- kt-lib/src/main/kotlin/Updater.kt
+++ kt-lib/src/main/kotlin/Updater.kt
@@ -4,7 +4,7 @@ import java.util.*
 // TODO: change /Ladsers/temp-files/ to /Ladsers/Passtable/
 
 /**
- * An object responsible for updating applications (jvm&android).
+ * An object responsible for updating applications (jvm & android).
  */
 object Updater {
     private val urlUpdate = URL("""https://raw.githubusercontent.com/Ladsers/temp-files/master/update.ini""")
@@ -13,28 +13,27 @@ object Updater {
     /**
      * Check for new versions of applications.
      *
-     * The search is performed by [verTag] in the file on remote server (GitHub).
+     * The search is performed by [platform] and [verTag] in the file on remote server (GitHub).
      * @return [0] – success: no update required, [1] – success: update is available,
      * [-1] – error: unable to parse the file on remote server.
      */
-    fun check(verTag: String): Int {
+    fun check(platform: String, verTag: String): Int {
         val isNeedUpdate: Boolean
-
-        val platform = verTag.substringAfterLast('.')
 
         try {
             val s = Scanner(urlUpdate.openStream())
             s.useDelimiter("[^\\S]")
             val lines = mutableListOf<String>()
             while (s.hasNextLine()) lines.add(s.next())
+
             isNeedUpdate = when (platform.toLowerCase()) {
                 "jvm" -> {
-                    lastVer = lines[3]
-                    verTag != lines[3]
+                    lastVer = lines[1]
+                    verTag != lines[1]
                 }
                 "apk" -> {
-                    lastVer = lines[5]
-                    verTag != lines[5]
+                    lastVer = lines[3]
+                    verTag != lines[3]
                 }
                 else -> {
                     lastVer = verTag
