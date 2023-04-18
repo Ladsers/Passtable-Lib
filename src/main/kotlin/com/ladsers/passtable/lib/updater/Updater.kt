bd06d670b0961fe08e1a7882192502ba08dae676
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2023-04-18 20:55:14 +0300

    Corrected comments.

--- src/main/kotlin/com/ladsers/passtable/lib/updater/Updater.kt
+++ src/main/kotlin/com/ladsers/passtable/lib/updater/Updater.kt
@@ -13,17 +13,22 @@ object Updater {
 
     private val source = URL("""https://ladsers.com/wp-content/uploads/passtable-appversion.json""")
 
+    /**
+     * The latest version tag. Default value before accessing the remote server: "unknown".
+     */
     var lastVer: String = "unknown"
         private set
 
     /**
      * Check for new versions of applications.
      *
-     * The search is performed by [platform] and [currentVerTag] in the file on remote server (GitHub).
-     * @return [0] – success: no update required, [1] – success: update is available,
-     * [-1] – error: unable to parse the file on remote server.
+     * The search is performed by [platform] in the Json file on remote server.
+     * The comparison is based on the [currentVerTag] and the latest one.
+     * @return result code.
+     * @see UpdaterCheckResult
      */
     fun check(platform: Platform, currentVerTag: String): UpdaterCheckResult {
+        /* Get data from file on the remote server */
         val jsonData: String
 
         try {
@@ -34,6 +39,7 @@ object Updater {
             return UpdaterCheckResult.CONNECTION_ERROR
         }
 
+        /* Parse the received data */
         val appVersion: AppVersion
 
         try {
@@ -43,6 +49,7 @@ object Updater {
             return UpdaterCheckResult.PARSING_ERROR
         }
 
+        /* Get the version tag */
         lastVer = appVersion.getVerTag(platform)
 
         return if (currentVerTag == lastVer) UpdaterCheckResult.UP_TO_DATE else UpdaterCheckResult.NEED_UPDATE
